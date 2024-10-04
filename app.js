3a18e31eb71ab58195d24734a7989cc30f9a0ae5
mrgnschndr <mrgnschndr@gmail.com>
2024-10-03 19:36:23 -0600

    Complete task 2

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const paragraph = document.querySelector('#content p');
 
 console.log(mainTitle);
 console.log(paragraph);
+
+
+// Task 2
+mainTitle.textContent = 'DOM Manipulation with JavaScript';
+paragraph.textContent = 'Hello, I am Morgan and I am learning DOM manipulation!';
