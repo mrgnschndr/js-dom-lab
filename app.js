4420302efcbb243df0e2445f8a735d5b676b8529
mrgnschndr <mrgnschndr@gmail.com>
2024-10-03 20:03:10 -0600

    Complete task 4

--- app.js
+++ app.js
@@ -16,3 +16,17 @@ const newItem = document.createElement('li');
 newItem.textContent = 'Newly added item';
 const itemList = document.getElementById('item-list');
 itemList.appendChild(newItem);
+
+
+// Task 4
+const addItemBtn = document.getElementById('add-item-btn');
+addItemBtn.addEventListener('click', function() {
+    const newItem = document.createElement('li');
+    newItem.textContent = 'Another new item';
+    itemList.appendChild(newItem);
+});
+
+const highlightBtn = document.getElementById('highlight-title-btn');
+highlightBtn.addEventListener('click', function() {
+    mainTitle.classList.toggle('highlight');
+});
